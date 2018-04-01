ebcceede8f249c5e72ac1bf6be09e8231817c22c
zhaolihang <625335512@qq.com>
2018-04-01 20:41:20 +0800

    test in 16bits code use  32bit register

--- src/a_test/mbr.asm
+++ src/a_test/mbr.asm
@@ -1,41 +1,37 @@
 
 
-SECTION mbr align=16 vstart=0x7c00
+SECTION mbr align=16 vstart=0
             [bits 16]
+            mov eax,0
+            mov es,eax
+            mov ds,eax
 
-            mov ax,0
-            mov ds,ax
-            mov es,ax
-            ;0x7c00以下是栈段
-            mov ax,0
-            mov ss,ax  
-            mov sp,0x7c00
-
-            mov ax,[es:gdt_baseP]
-            mov dx,[es:gdt_baseP+2]
-            mov bx,16
-            div bx
-            mov ds,ax;set data segment
+            mov eax,[es:gdt_p+0x7c00+2]
+            shr eax,4
+            mov ds,eax
 
             ; first null gdt
             mov dword [0x00],0x00
             mov dword [0x04],0x00
 
-            ;this code segment
-            mov dword [0x08],0x7c0001ff ;段基址是0x7c00 
-            mov dword [0x0c],0x00409800
+            mov dword [0x08],0x0000ffff ;数据段 粒度4k 0~4GB线性空间 
+            mov dword [0x0c],0x00cf9200  
+
+            ; code segment
+            mov dword [0x10],0x7c0001ff    ;基地址为0x00007c00，512字节 
+            mov dword [0x14],0x00409800    ;粒度为1个字节，代码段描述符 
             
-            ; video segment
-            mov dword [0x10],0x8000ffff     
-            mov dword [0x14],0x0040920b
+            ;创建以上代码段的别名描述作为数据段
+            mov dword [0x18],0x7c0001ff    ;基地址为0x00007c00，512字节
+            mov dword [0x1c],0x00409200    ;粒度为1个字节，数据段描述符
 
-            ;stack segment
-            mov dword [0x18],0x00007a00; 粒度=0 byte type=0010 向下拓展 基地址0x00000000 界限是0x7a00 
-            mov dword [0x1c],0x00409600 ;即 0x0000ffff → 0x00007a00 属于此段
+            ;stack segment  0x7c00 
+            mov dword [0x20],0x7c00fffe
+            mov dword [0x24],0x00cf9600
 
-            mov word [es:gdt_baseSizeP],31 ;最大索引值 按bit计算
+            mov word [es:gdt_p+0x7c00],5*8-1 ;最大索引值 按bit计算
 
-            lgdt [es:gdt_baseSizeP]
+            lgdt [es:gdt_p+0x7c00]
 
 fastA20:    in al,0x92                         ;南桥芯片内的端口 
             or al,0000_0010B
@@ -49,58 +45,50 @@ fastA20:    in al,0x92                         ;南桥芯片内的端口
 
             ;dword 说明偏移量是32位 由于这条代码是在16位模式下编译的所以有后缀0x66
             ;已经进入了保护模式所以段前缀表示段选择子 1号段即当前代码段 进行跳转的目的是刷新代码段寄存器缓存，清空流水线 
-            ;由于 vstart=0x7c00 并且代码段的基址是0x7c00 所以flush-0x7c00 才是真正的偏移地址
-            jmp dword 0_0000_0000_0001_000b:(flush-0x7c00)  
+            jmp dword 0_0000_0000_0010_000b:flush  ;加载第2号段
 
             [bits 32]
 flush:
 _32Start:;一下代码是在32位模式下运行的
-            mov ax,0_0000_0000_0010_000b;加载第2号段 即显卡数据段
-            mov ds,ax
+            mov eax,0_0000_0000_0011_000b;加载第3号段
+            mov ds,eax
 
-            ; 在屏幕上显示字符 说明成功的进入了保护模式
-            mov byte [0x00],'P'  
-            mov byte [0x02],'r'
-            mov byte [0x04],'o'
-            mov byte [0x06],'t'
-            mov byte [0x08],'e'
-            mov byte [0x0a],'c'
-            mov byte [0x0c],'t'
-            mov byte [0x0e],' '
-            mov byte [0x10],'m'
-            mov byte [0x12],'o'
-            mov byte [0x14],'d'
-            mov byte [0x16],'e'
-            mov byte [0x18],' '
-            mov byte [0x1a],'O'
-            mov byte [0x1c],'K'
-            mov byte [0x1e],'!'
-            mov byte [0x20],'!'
-            mov byte [0x22],'!'
-            mov byte [0x24],' '
-            mov byte [0x26],' '
+            ;设置 数据段指向 0~4GB的空间
+            mov eax,0_0000_0000_0001_000b ;加载第1号段
+            mov es,eax
+            mov fs,eax
+            mov gs,eax
 
-            ;test 32 prodect mode stack opration
-            mov ax,0_0000_0000_0011_000b
-            mov ss,ax
-            mov esp,0x7c00
+            mov eax,0_0000_0000_0100_000b ;加载第4号段 stack segment
+            mov ss,eax
+            xor esp,esp
 
-            mov eax,esp
-            push byte '.'  ;压入的是字节 编译之后还是4字节的 栈工作在32模式下即使压入的一个字节单数esp指针已然要减4
-            sub eax,4
-            cmp eax,esp
-            jnz infi
-            pop eax
-            mov byte [0x28],al
-            mov byte [0x2a],' '
-            mov byte [0x2c],' '
+            mov byte [es:0x0b8000+0x00],'P'  
+            mov byte [es:0x0b8000+0x02],'r'
+            mov byte [es:0x0b8000+0x04],'o'
+            mov byte [es:0x0b8000+0x06],'t'
+            mov byte [es:0x0b8000+0x08],'e'
+            mov byte [es:0x0b8000+0x0a],'c'
+            mov byte [es:0x0b8000+0x0c],'t'
+            mov byte [es:0x0b8000+0x0e],' '
+            mov byte [es:0x0b8000+0x10],'m'
+            mov byte [es:0x0b8000+0x12],'o'
+            mov byte [es:0x0b8000+0x14],'d'
+            mov byte [es:0x0b8000+0x16],'e'
+            mov byte [es:0x0b8000+0x18],' '
+            mov byte [es:0x0b8000+0x1a],'O'
+            mov byte [es:0x0b8000+0x1c],'K'
+            mov byte [es:0x0b8000+0x1e],'!'
+            mov byte [es:0x0b8000+0x20],'!'
+            mov byte [es:0x0b8000+0x22],'!'
+            mov byte [es:0x0b8000+0x24],' '
+            mov byte [es:0x0b8000+0x26],' '
 
 infi:       jmp near infi
 
 
-gdt_baseSizeP:  
+gdt_p:  
             dw 0
-gdt_baseP:  
             dd 0x00007e00
 
             times 510-($-$$) db 0
